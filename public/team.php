ea4802abe7bcb60318e0cd81b5f9f2445e7feaa7
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-14 11:58:45 -0300

    Update team.php

--- public/team.php
+++ public/team.php
@@ -3,33 +3,27 @@ require("header/cab.php");
 ?>
 <div class="container">
     <div class="row">
-        <div>
-            <img src="/assets/logo/logo_humanidados.png" class="img-fluid" style="height: 100px">
-
+        <div class="col-lg-10">
             <h1>Equipe do Projeto</h1>
 
-            <div class="col-lg-12">
-                <h4>Fábio Mascarenhas e Silva</h4>
-                Coordenador do Projeto
-            </div>
+            <h4>Fábio Mascarenhas e Silva</h4>
+            Coordenador do Projeto
 
-            <div class="col-lg-10">
-                <h4>Rene Faustino Gabriel Junior</h4>
-                Pesquisador
+            <h4>Rene Faustino Gabriel Junior</h4>
+            Pesquisador
 
-                <h4>Jesús Pascual Mena Chalco</h4>
-                Pesquisador
+            <h4>Jesús Pascual Mena Chalco</h4>
+            Pesquisador
 
-                <h4>Thiarles Klaza Ferreira</h4>
-                Bolsista Iniciação Científicas
+            <h4>Thiarles Klaza Ferreira</h4>
+            Bolsista Iniciação Científicas
 
-                <h4>Bolsista 2</h4>
-                Bolsista Iniciação Científicas
-            </div>
+            <h4>Bolsista 2</h4>
+            Bolsista Iniciação Científicas
+        </div>
 
-            <div class="col-lg-2">
-                <img src="/assets/logo/logo_humanidados.png" class="img-fluid">
-            </div>
+        <div class="col-lg-2">
+            <img src="/assets/logo/logo_humanidados.png" class="img-fluid">
         </div>
     </div>
 </div>
