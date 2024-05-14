c71121ee153b841859fa8adc55c0f619aeb21df7
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-14 12:01:10 -0300

    Update team.php

--- public/team.php
+++ public/team.php
@@ -6,19 +6,19 @@ require("header/cab.php");
         <div class="col-lg-10">
             <h1>Equipe do Projeto</h1>
 
-            <label>Coordenador do Projeto</label>
+            <label class="mt-2">Coordenador do Projeto</label>
             <h4>Fábio Mascarenhas e Silva</h4>
 
-            <label>Pesquisador</label>
+            <label class="mt-2">Pesquisador</label>
             <h4>Rene Faustino Gabriel Junior</h4>
 
-            <label>Pesquisador</label>
+            <label class="mt-2">Pesquisador</label>
             <h4>Jesús Pascual Mena Chalco</h4>
 
-            <label>Bolsista Iniciação Científicas</label>
+            <label class="mt-2">Bolsista Iniciação Científicas</label>
             <h4>Thiarles Klaza Ferreira</h4>
 
-            <label>Bolsista Iniciação Científicas</label>
+            <label class="mt-2">Bolsista Iniciação Científicas</label>
             <h4>Bolsista 2</h4>
 
         </div>
