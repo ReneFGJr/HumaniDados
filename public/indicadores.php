82c3bfb31c1f834c332b6d0a81b399afe43ac3a1
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-12 08:45:03 -0300

    Update indicadores.php

--- public/indicadores.php
+++ public/indicadores.php
@@ -1,22 +1,28 @@
 <?php
 require("header/cab.php");
 
-$indicadores = ['?ind=1'=>'Produção Científica', '?ind=2' => 'Produção Artistica'];
+$indicadores = ['?ind=1' => 'Produção Científica', '?ind=2' => 'Produção Artistica'];
 ?>
 <div class="container">
     <div class="row">
         <div class="text-center">
-            <img src="/assets/logo/logo_humanidados.png" style="height: 100px">
-        </div>
-        <?php
-        foreach ($indicadores as $link => $label) { ?>
-            <div class="col-12 col-sm-3">
-                    <?php echo $label; ?>
+            <div class="col-12 col-lg-3">
+                <img src="/assets/logo/logo_humanidados.png" style="height: 100px">
             </div>
-        <?php } ?>
-    </div>
-</div>
+            <div class="col-12 col-lg-9">
+                <h1 class="text-center">PAINEL</h1>
+            </div>
 
-<?php
-require("header/foot.php");
-?>
+            </div>
+            <?php
+            foreach ($indicadores as $link => $label) { ?>
+                <div class="col-12 col-sm-3">
+                    <?php echo $label; ?>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
+    <?php
+    require("header/foot.php");
+    ?>
