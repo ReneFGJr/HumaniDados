1f372c4d1da2cb2e07fe800c8a3211afd544ef38
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-17 10:39:08 -0300

    Update foot.php

--- public/header/foot.php
+++ public/header/foot.php
@@ -4,7 +4,7 @@
             <img src="/assets/logo/logo-ufabc.jpg" class="img-fluid mb-2">
             <img src="/assets/logo/logo-ufpe.jpg" class="img-fluid mb-2">
             <img src="/assets/logo/logo-ufrgs.png" class="img-fluid mb-2">
-            <br>&copy; <?php echo date("Y"); ?>&copy;
+            <br>&copy; <?php echo date("Y"); ?>
         </div>
     </div>
 </div>
