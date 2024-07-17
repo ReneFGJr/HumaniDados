833b997d9f63d0de5b7d368fc292993107cf0a5d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-17 10:48:58 -0300

    Update producao.php

--- public/producao.php
+++ public/producao.php
@@ -5,6 +5,9 @@ require("header/cab.php");
     <div class="row">
         <div class="col-lg-10">
             <h1>Produção sobre o projeto</h1>
+
+            <h2>Eventos</h2>
+            <p>MASCARENHAS, F. S. xxxx</p>
         </div>
     </div>
 
