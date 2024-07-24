7be2ed8f3268aefd7ef150c41f9da61e4f7f111a
Thiarles-k <thiarlesklaza@gmail.com>
2024-07-24 09:59:36 -0300

    Update team.php

--- public/team.php
+++ public/team.php
@@ -8,19 +8,24 @@ require("header/cab.php");
 
             <label class="mt-2">Coordenador do Projeto</label>
             <h4>Fábio Mascarenhas e Silva</h4>
+            <p><a href="http://lattes.cnpq.br/9023999545198140"> Currículo Lattes</a></p>
 
             <label class="mt-2">Pesquisador</label>
             <h4>Rene Faustino Gabriel Junior</h4>
+            <p><a href="http://lattes.cnpq.br/5900345665779424"> Currículo Lattes</a></p>
 
             <label class="mt-2">Pesquisador</label>
             <h4>Jesús Pascual Mena Chalco</h4>
+            <p><a href="http://lattes.cnpq.br/4727357182510680"> Currículo Lattes</a></p>
 
             <label class="mt-2">Bolsista Iniciação Científicas</label>
             <h4>Thiarles Klaza Ferreira</h4>
+            <p><a href="http://lattes.cnpq.br/9844500379884281"> Currículo Lattes</a></p>
 
             <label class="mt-2">Bolsista Iniciação Científicas</label>
             <h4>Luana Bomfim</h4>
-
+            <p><a href="http://lattes.cnpq.br/6765749494836819"> Currículo Lattes</a></p>
+            
         </div>
 
         <div class="col-lg-2">
