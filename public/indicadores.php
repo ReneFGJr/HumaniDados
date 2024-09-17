8bd0bb9075cb33cc2a8843ede4e24524d3469f2f
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-09-17 15:09:34 -0300

    Update indicadores.php

--- public/indicadores.php
+++ public/indicadores.php
@@ -4,11 +4,10 @@ require("header/cab.php");
 $ind = $_GET['ind'];
 
 
-switch($ind)
-    {
-        default:
-            $indicadores = ['?ind=0' => 'Pesquisadores', '?ind=1' => 'Produção Científica', '?ind=2' => 'Produção Artistica', '?ind=3' => 'Produção Técnica'];
-            echo '
+switch ($ind) {
+    default:
+        $indicadores = ['?ind=0' => 'Pesquisadores', '?ind=1' => 'Produção Científica', '?ind=2' => 'Produção Artistica', '?ind=3' => 'Produção Técnica'];
+        echo '
                 <div class="container">
                     <div class="row">
                         <div class="col-12 col-lg-3">
@@ -18,31 +17,32 @@ switch($ind)
                             <h1 class="text-center">PAINEL</h1>
                         </div>';
 
-            foreach ($indicadores as $link => $label) {
-                echo '<div class="box col-12 col-sm-3 border border-secondary shadow rounded p-5 big text-center m-3" onclick="goURL(\''.$link.'\');">';
-                echo $label;
-                echo '</div>';
-                echo '</div>';
-            }
-            break;
-        case '0':
-            echo '<h1>Pesquisador</h1>';
-            break;
-    }
+        foreach ($indicadores as $link => $label) {
+            echo '<div class="box col-12 col-sm-3 border border-secondary shadow rounded p-5 big text-center m-3" onclick="goURL(\'' . $link . '\');">';
+            echo $label;
+            echo '</div>';
+        }
+
+        echo '</div>';
+        break;
+    case '0':
+        echo '<h1>Pesquisador</h1>';
+        break;
+}
 
 
 require("header/foot.php");
 ?>
 
 <script>
-function goURL(url) {
-    window.location.href = url;
-}
+    function goURL(url) {
+        window.location.href = url;
+    }
 </script>
 
 <style>
-.box:hover {
-    background-color: #F8F8F8;
-    cursor: pointer;
-}
+    .box:hover {
+        background-color: #F8F8F8;
+        cursor: pointer;
+    }
 </style>
