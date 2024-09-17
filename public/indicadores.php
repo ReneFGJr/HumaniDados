22a330086f3fb28639c1fe18b3470b973069b12d
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-09-17 16:10:03 -0300

    Update indicadores.php

--- public/indicadores.php
+++ public/indicadores.php
@@ -31,16 +31,12 @@ switch ($ind) {
         echo '<h1>Pesquisador</h1>';
         echo '<iframe src="https://dashboard.brapci.inf.br/goto/2c166350-7521-11ef-8ce1-a98ecc8d01af" height="1600" width="800"></iframe>';
         echo '</div></div>';
+        echo '<a href="datasets/D_PERSON.csv">MICRO DADOS</a>';
         break;
     case '1':
         echo '<div class="container"><div class="row">';
-        echo '<div class="col-12">';
         echo '<h1>Produção Ciêntífica</h1>';
         echo '<iframe src="https://dashboard.brapci.inf.br/goto/2c166350-7521-11ef-8ce1-a98ecc8d01af" height="1600" width="800"></iframe>';
-        echo '</div>';
-        echo '<div class="col-12">';
-        echo '<a href="datasets/D_PRODUCAO.csv">MICRO DADOS</a>';
-        echo '</div>';
         echo '</div></div>';
         break;
     case '2':
@@ -48,17 +44,14 @@ switch ($ind) {
         echo '<h1>Produção Artística</h1>';
         echo '<iframe src="https://dashboard.brapci.inf.br/goto/5c630380-7524-11ef-8ce1-a98ecc8d01af" height="1600" width="800"></iframe>';
         echo '</div></div>';
+        echo '<a href="datasets/D_PRODUCAO.csv">MICRO DADOS</a>';
         break;
     case '3':
         echo '<div class="container"><div class="row">';
-        echo '<div class="col-12">';
         echo '<h1>Produção Técnica</h1>';
         echo '<iframe src="https://dashboard.brapci.inf.br/goto/2c166350-7521-11ef-8ce1-a98ecc8d01af" height="1600" width="800"></iframe>';
-        echo '</div>';
-        echo '<div class="col-12">';
-        echo '<a href="datasets/D_PRODUCAO.csv">MICRO DADOS</a>';
-        echo '</div>';
         echo '</div></div>';
+        echo '<a href="datasets/D_PRODUCAO.csv">MICRO DADOS</a>'
         break;
 
 }
