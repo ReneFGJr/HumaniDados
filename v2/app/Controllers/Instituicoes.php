cdac22c1e913771d20549f93db7444f9d1b5197d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-02-18 07:10:31 -0300

    Update Instituicoes.php

--- v2/app/Controllers/Instituicoes.php
+++ v2/app/Controllers/Instituicoes.php
@@ -32,10 +32,6 @@ class Instituicoes extends BaseController
         $InstituicoesModel = new \App\Models\InstituicaoLattesModel();
         $data['instituicao'] = $InstituicoesModel->le($id);
 
-        echo '<h1>'.$id.'</h1>';
-
-        pre($data);
-
         echo view('layout/header');
         echo view('instituicao/view', $data);
         echo view('layout/footer');
