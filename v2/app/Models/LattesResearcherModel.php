a9993a25a72273a37b9de41b16bd8722345b7ac8
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-03-03 15:11:51 -0300

    Update LattesResearcherModel.php

--- v2/app/Models/LattesResearcherModel.php
+++ v2/app/Models/LattesResearcherModel.php
@@ -193,8 +193,7 @@ class LattesResearcherModel extends Model
         {
             $this->mudarStatusColetas();
             $pesquisadores = $this
-                ->where('situacao_coleta', 'coletado')
-                ->findAll();
+                ->findAll(10);
             $total = count($pesquisadores);
             $processados = 0;
 
