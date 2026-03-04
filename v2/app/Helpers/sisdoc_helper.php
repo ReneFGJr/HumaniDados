5aa10a43128a82482d4005eac72c94a238d2b230
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-03-04 10:39:52 -0300

    Update sisdoc_helper.php

--- v2/app/Helpers/sisdoc_helper.php
+++ v2/app/Helpers/sisdoc_helper.php
@@ -132,3 +132,18 @@ function treeToHtmlList(array $tree, $n=0): string
     $html .= "</ul>";
     return $html;
 }
+
+function get($var)
+    {
+            $vlr = '';
+        if (isset($_GET[$var])) {
+            $vlr = $_GET[$var];
+        } else {
+            if (isset($_POST[$var])) {
+                $vlr = $_POST[$var];
+            }
+        }
+        $vlr = str_replace("'", "", $vlr);
+        $vlr = str_replace('"', "", $vlr);
+        return $vlr;
+    }
